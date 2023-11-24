27e9a1a401db3fef5f3e0d0a8fb7c058d794c5a1
Krauberger.ZhYu <krauberger.zhyu@dns-shop.ru>
2023-11-24 18:08:06 +1000

    add view about_me

--- homework_05/app.py
+++ homework_05/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
@@ -6,6 +7,14 @@ app = Flask(__name__)
 @app.get("/")
 def get_index():
     return render_template("index.html")
+
+
+@app.route("/about_me/", endpoint="about_me")
+def get_my_name():
+    name = request.form.get("my-name")
+    print(name)
+
+
 
 
 @app.get("/about/", endpoint="about")
