fbf94b4559691a20f464fb11da654a9251820ab1
Krauberger.ZhYu <krauberger.zhyu@dns-shop.ru>
2023-11-28 15:11:29 +1000

    add new columns to model user

--- homework_06/models/user.py
+++ homework_06/models/user.py
@@ -5,7 +5,9 @@ from .database import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     psw = db.Column(db.String(500), nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
