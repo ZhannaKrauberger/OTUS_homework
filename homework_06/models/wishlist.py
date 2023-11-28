c9089c2e557f392db7c29922a1717d3974b36066
Krauberger.ZhYu <krauberger.zhyu@dns-shop.ru>
2023-11-28 14:49:05 +1000

    add relationship with user

--- homework_06/models/wishlist.py
+++ homework_06/models/wishlist.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from .database import db
 
 
@@ -6,5 +8,9 @@ class WishList(db.Model):
     name = db.Column(db.String(120), nullable=False, default="My wishes")
     description = db.Column(db.Text)
     secret = db.Column(db.Boolean, default=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
-    author = db.Column(db.Integer)  # связь с юзером
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))  # связь с юзером
+
+    def __repr__(self):
+        return f"wishlist: id={self.id} name={self.name} user: id={self.user_id}"
