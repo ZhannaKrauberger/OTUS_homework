d7e7c23d4b2042e9125b020c32ee31fca8f495fc
Krauberger.ZhYu <krauberger.zhyu@dns-shop.ru>
2023-11-28 14:02:39 +1000

    add model User

--- homework_06/app.py
+++ homework_06/app.py
@@ -1,11 +1,18 @@
 from os import getenv
 
 from flask import Flask, render_template
+from flask_migrate import Migrate
+
+from models.database import db
 
 app = Flask(__name__)
 
 config_name = getenv("CONFIG NAME", "DevelopmentConfig")
 app.config.from_object(f"config.{config_name}")
+
+
+db.init_app(app=app)
+migrate = Migrate(app=app, db=db)
 
 
 @app.route("/")
