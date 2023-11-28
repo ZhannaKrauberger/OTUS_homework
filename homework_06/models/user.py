c08f2198e7ee53d0e28439555973dccd9dbb367a
Krauberger.ZhYu <krauberger.zhyu@dns-shop.ru>
2023-11-28 18:32:21 +1000

    rename psw > password

--- homework_06/models/user.py
+++ homework_06/models/user.py
@@ -9,7 +9,7 @@ class User(db.Model):
     last_name = db.Column(db.String(120), nullable=False)
     username = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
-    psw = db.Column(db.String(500), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
