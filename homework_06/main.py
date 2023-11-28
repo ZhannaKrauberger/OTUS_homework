27b849eea503cc5368b26f64892a08e9304fc0fc
Krauberger.ZhYu <krauberger.zhyu@dns-shop.ru>
2023-11-28 12:17:12 +1000

    add main.py

--- homework_06/main.py
+++ homework_06/main.py
@@ -1,16 +1,4 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+from app import app
 
 
 if __name__ == "__main__":
